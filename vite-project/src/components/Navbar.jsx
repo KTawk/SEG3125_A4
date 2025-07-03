3ed3beebc5cc60ed8ae38997baee48c41f7b999c
Suhas Chimmapudi <schim055@uottawa.ca>
2025-07-03 17:41:05 -0400

    navbar change

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -4,29 +4,54 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav classname = 'bg=white shadow-md'>
-      <div className='container mx-auto px-4 md: px-16 lg:px-24 py-4 flex justify-between items-center'>
-        <div className='text-lg font-bold'>
-          <Link to="/">e-commerce</Link>
+    <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
+
+
+        <div className="text-2xl font-semibold">
+          <Link to="/">
+            e-<span className="font-bold">SHOP</span>
+          </Link>
         </div>
-        <div className='relative flex-1 mx-4'>
+
+
+        <div className="relative w-full max-w-md mx-4">
           <form>
-            <input type="text" placeholder="Search Product"
-            clasName='w-full border py-2 px-4'/>
-            <FaSearch className='absolute top-3 right-3 text-red-500'></FaSearch>
+            <input
+              type="text"
+              placeholder="Search Product"
+              className="w-full border border-gray-300 py-2 px-4 rounded-md"
+            />
+            <FaSearch className="absolute top-3 right-3 text-red-500" />
           </form>
         </div>
-        <div className='flex items-center space-x-4'>
+
+        <div className="flex items-center space-x-4 text-gray-700">
           <Link to="/cart">
-            <FaShoppingCart className='text-xl'/>
+            <FaShoppingCart className="text-xl text-purple-500" />
           </Link>
-          <button className='hidden md:block'>
-              Login | Register
+          <button className="hidden md:block bg-gray-100 px-3 py-1 rounded-md shadow-sm">
+            Login | Register
           </button>
-          <button classname='block md:hidden'>
-              <FaUser></FaUser>
+          <button className="block md:hidden">
+            <FaUser />
           </button>
         </div>
+      </div>
+
+      <div className='flex items-center justify-center space-x-10 py-4 text-sm font-bold'>
+        <Link className='hover:underline'>
+          Home 
+        </Link>
+        <Link className='hover:underline'>
+          Shop
+        </Link>
+        <Link className='hover:underline'>
+          Contact 
+        </Link>
+        <Link className='hover:underline'>
+          About
+        </Link>
       </div>
     </nav>
   );
