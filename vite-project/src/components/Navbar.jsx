7d59350ef12297360fd2fdecd092601ba02997b4
Suhas Chimmapudi <schim055@uottawa.ca>
2025-07-03 17:17:56 -0400

    navbar started

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import { FaSearch, FaShoppingCart, FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <div>
+    <nav classname = 'bg=white shadow-md'>
+      <div className='container mx-auto px-4 md: px-16 lg:px-24 py-4 flex justify-between items-center'>
+        <div className='text-lg font-bold'>
           <Link to="/">e-commerce</Link>
         </div>
-        <div>
+        <div className='relative flex-1 mx-4'>
           <form>
-            <input type="text" placeholder="Search" />
+            <input type="text" placeholder="Search Product"
+            clasName='w-full border py-2 px-4'/>
+            <FaSearch className='absolute top-3 right-3 text-red-500'></FaSearch>
           </form>
+        </div>
+        <div className='flex items-center space-x-4'>
+          <Link to="/cart">
+            <FaShoppingCart className='text-xl'/>
+          </Link>
+          <button className='hidden md:block'>
+              Login | Register
+          </button>
+          <button classname='block md:hidden'>
+              <FaUser></FaUser>
+          </button>
         </div>
       </div>
     </nav>
